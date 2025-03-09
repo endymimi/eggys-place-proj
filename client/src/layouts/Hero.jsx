c39a5630f96678afa4e43768f0a1521df0cfde26
endy mimi <ighobuendy@gmail.com>
2025-03-09 03:24:36 -0700

    features:fixes

--- client/src/layouts/Hero.jsx
+++ client/src/layouts/Hero.jsx
@@ -2,23 +2,43 @@ import React from 'react'
 import burgerImg from "../assets/burgerBg.svg";
 
 const Hero = () => {
+
   return (
     <>
-    <main  className='bg-[url(https://res.cloudinary.com/ds0a0s3k3/image/upload/v1741087159/Hero-img_mxynhn.svg)] bg-cover bg-no-repeat h-[600px] bg-center   w-full '>
+    <main  className='bg-[url(https://res.cloudinary.com/ds0a0s3k3/image/upload/v1741087159/Hero-img_mxynhn.svg)] bg-cover bg-no-repeat h-[600px] bg-center   w-full  lg:h-[600px] flex justify-center items-center opacity-93"'>
     
-
+   
+        
+       
+        <section className="w-[700px] flex flex-col items-center text-center gap-[40px] ">
+          
+          
+          <div className="p-2 lg:p-0 ">
+           
+          <div className="p-2 lg:p-0">
+            <h1 className="text-white whitespace-pre-wrap  md:mx- lg:text-[56px] text-[50px] font-[500] lg:tracking-[7%] md:leading-[74.09px]">
+              The{" "}
+              <span className="text-[#B67B0F] font-[800] text-[56px] font-Rammetto md:leading-[94.58px] md:tracking-[7%]">
+                ZING
+              </span>{" "}
+              is in every bite{" "}
+            </h1>
+            <p className="text-white md:text-[28px] md:leading-[37.04px] font-[500]">
+              Savor the flavor! Explore our delicious menu and order now for a
+              taste sensation!
+            </p>
+          </div>
+        </div>
     
-    <div className='container mx-auto justify-center items-center flex flex-col md:w-[650px] md:h-[110px]  '>
-    <h4 className='text-[#FBFBFB]  font-RedHat font-[400] text-[28px] md:text-[56px] mt-30 md:mt-60'>The <span className='text-[#B67B0F] text-[35px] md:text-[56px] font-[500] font-Rammetto  '>ZING</span> in every bites</h4>
-    <p className='text-[#FBFBFB] font-RedHat md:text-[23px] md:font-[400] md:tracking-wide tracking-tighter mx-4'> Savor the flavour! Explore our delicious menu  and order <span className='md:mx-40 mx-20' > now for a taste sensation!</span></p>
-    
-    </div>
-    <div className='container mx-auto justify-center items-center my-35 md:my-55 md:ml-7 md:gap-6 gap-2 flex'>
-    <button className='  md:block btn bg-[#B67B0F] rounded-[31px] text-[#F0F0F0] md:w-55 md:h-17 w-45 h-14'>Order Now!</button>
-    <button className='  md:block btn bg-[#FBFBFB] rounded-[32px] text-[#100101] md:w-55 w-40 md:h-17 h-15'>View Menu</button>
-    </div>
-    
-            
+        <div className="hidden lg:flex gap-[12px]">
+            <button className="bg-[#B67B0F] rounded-[31px] py-[15px] text-white px-[56px] text-[20px] leading-[26.46px]">
+              Order Now!
+            </button>
+            <button className="bg-[#FBFBFB] rounded-[32px] py-[15px] px-[20px] leading-[26.46px] text-[20px] font-[500]">
+              View Menu
+            </button>
+          </div>
+     </section>       
     </main>
     </>
   )
